f60235c1e04b8f5e9974317f43e76ac8dd29aeac
senafsc <senalufer@gmail.com>
2022-08-25 18:13:12 -0700

    adicionando icones, images e implementando issue model

---
+++ AdrianoAndersonGustavoLuisPaulo/Shared/Model/Issue.swift
@@ -0,0 +1,17 @@
+//
+//  Issue.swift
+//  AdrianoAndersonGustavoLuisPaulo
+//
+//  Created by user219898 on 8/25/22.
+//
+
+import Foundation
+
+struct Issue: Codable {
+    let title: String
+    let id: String
+    let date: String
+    let description: String
+    let location: String
+    let solved: String
+}
