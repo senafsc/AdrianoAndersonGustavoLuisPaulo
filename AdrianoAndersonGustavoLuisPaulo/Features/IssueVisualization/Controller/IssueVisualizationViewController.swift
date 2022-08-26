1ac258f2dbb946776bd50672e0ff3fd0e6fa1700
senafsc <senalufer@gmail.com>
2022-08-25 22:23:15 -0700

    Adicionando componentes para insercao de dados e criando outlets na issueVisualizationController

--- AdrianoAndersonGustavoLuisPaulo/Features/IssueVisualization/Controller/IssueVisualizationViewController.swift
+++ AdrianoAndersonGustavoLuisPaulo/Features/IssueVisualization/Controller/IssueVisualizationViewController.swift
@@ -9,9 +9,20 @@ import UIKit
 
 class IssueVisualizationViewController: UIViewController {
 
+    @IBOutlet weak var textFieldTitle: UITextField!
+    @IBOutlet weak var textFieldDate: UITextField!
+    @IBOutlet weak var textFieldAdress: UITextField!
+    @IBOutlet weak var textViewDescription: UITextView!
+    @IBOutlet weak var buttonSave: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+        print("Clicou no botão SALVAR")
     }
 
 
